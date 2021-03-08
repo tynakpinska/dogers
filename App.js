9b627fc94444840dfc6e519fa61adb35de7b5b4c
tynakpinska <m.k.kepinska@gmail.com>
2021-03-08 17:52:35 +0100

    initial configuration with babel and webpack

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Components/Nav";
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      Dogers
+    </div>
+  );
+};
+
+export default App;
