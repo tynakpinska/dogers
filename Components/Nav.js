b512592b523dcb4b58c064668609c1fb457c62e1
tynakpinska <m.k.kepinska@gmail.com>
2021-04-07 19:02:19 +0200

    use NavLink with activeClassName

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Nav.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import Content from "../Components/Content";
-import Home from "../Routes/Home";
 import LogIn from "../Routes/LogIn";
 import Register from "../Routes/Register";
 
@@ -13,13 +17,13 @@ const Nav = ({ open }) => {
         <nav>
           <ul className="menu">
             <li className="menu__item">
-              <Link to="/">Home</Link>
+              <NavLink to="/" activeClassName="active">Home</NavLink>
             </li>
             <li className="menu__item">
-              <Link to="/login">Log In</Link>
+              <NavLink to="/login" activeClassName="active">Log In</NavLink>
             </li>
             <li className="menu__item">
-              <Link to="/register">Register</Link>
+              <NavLink to="/register" activeClassName="active">Register</NavLink>
             </li>
           </ul>
         </nav>
@@ -38,7 +42,7 @@ const Nav = ({ open }) => {
         <Route path="/walker">
           <Content open={open} content="walker" />
         </Route>
-        <Route path="/">
+        <Route exact path="/">
           <Content open={open} content="home" />
         </Route>
       </Switch>
