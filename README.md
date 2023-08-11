79596578c095c8bd63086a46eb88a0bbbe958c96
tynakpinska <m.k.kepinska@gmail.com>
2023-08-11 13:43:26 +0200

    add state to LogIn component

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Work in progress...
 
 I'm taking mobile first approach.
+
+Learning BEM methodology.
--- Routes/LogIn/LogIn.js
+++ Routes/LogIn/LogIn.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
 
 const LogIn = ({ open }) => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className={open ? "login open" : "login"}>
       <h2 className="login__header">Log in</h2>
       <form className="login__form">
         <label className="login__label" htmlFor="name">
-          Login:{" "}
+          Login:
         </label>
-        <input className="login__input" type="text" name="name" required />
+        <input
+          className="login__input"
+          type="text"
+          name="name"
+          value={login}
+          onChange={e => setLogin(e.target.value)}
+          required
+        />
         <label className="login__label" htmlFor="password">
-          Password:{" "}
+          Password:
         </label>
-        <input className="login__input" type="password" name="password" required />
+        <input
+          className="login__input"
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
         <input className="login__input--button" type="submit" value="Log in" />
       </form>
     </div>
